147149a1b40f8d3dea3621351f53cbbf5f36c8db
ambrosektal <ambrosektal@users.noreply.github.com>
2020-11-11 13:18:03 -0500

    Create binger.ps1

---
+++ binger.ps1
@@ -0,0 +1,34 @@
+[Object]$Random = New-Object System.Random
+
+$js_array=("JS Output","JS Statements","JS Syntax","JS Comments","JS Variables","JS Operators","JS Arithmetic","JS Assignment","JS Data Types","JS Functions","JS Objects","JS Events","JS Strings","JS String Methods","JS Numbers","JS Number Methods","JS Arrays","JS Array Methods","JS Array Sort","JS Array Iteration","JS Dates","JS Date Formats","JS Date Get Methods","JS Date Set Methods","JS Math","JS Random","JS Booleans","JS Comparisons","JS Conditions","JS Switch","JS Loop For","JS Loop While","JS Break","JS Type Conversion","JS Bitwise","JS RegExp","JS Errors","JS Scope","JS Hoisting","JS Strict Mode","JS this Keyword","JS Let","JS Const","JS Arrow Function","JS Classes","JS Debugging","JS Style Guide","JS Best Practices","JS Mistakes","JS Performance","JS Reserved Words","JS Versions")
+
+$js_array_2=("What is the difference between Node.js and JavaScript?","What is Node.js?","What is the working of Node.js.","Where is Node.js used?","What is the difference between Node.js and Angular?","Why is Node.js single-threaded?","What are the different API functions supported by Node.js?","What is the difference between synchronous and asynchronous functions?","What is the control flow function?","Why is Node.js so popular these days?","What is an event loop in Node.js?","What are the asynchronous tasks that should occur in an event loop?","What is the order of execution in control flow statements?","What are the input arguments for an asynchronous queue?","Are there any disadvantages to using Node.js?","What is the primary reason to use the event-based model in Node.js?","How can you import external libraries into Node.js?","What is meant by event-driven programming in Node.js?","What is the difference between Ajax and Node.js?","What is the framework that is used majorly in Node.js today?","What are the security implementations that are present in Node.js?","What is the meaning of a test pyramid?","What is libuv?","Why does Google use the V8 engine for Node.js?","What is the difference between spawn and fork methods in Node.js?","What is the use of middleware in Node.js?","What are global objects in Node.js?","Why is assert used in Node.js?","What are stubs in Node.js?","How is a test pyramid implemented using the HTML API in Node.js?","Why is a buffer class used in Node.js?","Why is ExpressJS used?","What is the use of the connect module in Node.js?","What are streams in Node.js?","What are the types of streams available in Node.js?","What is the use of REPL in Node.js?","What is meant by tracing in Node.js?","Where is package.json used in Node.js?","What is the difference between readFile and createReadStream in Node.js?","What is the use of the crypto module in Node.js?","What is a passport in Node.js?","How to get information about a file in Node.js?","How does the DNS lookup function work in Node.js?","What is the use of EventEmitter in Node.js?","What is the difference between setImmediate() and setTimeout()?","What is the use of module.exports in Node.js?","Why do you think you are the right fit for this Node.js role?","Do you have any past Node.js work experience?","Do you have any experience working in the same industry as ours?","Do you have any certification to boost your candidature for this Node.js role?" )
+
+$js_array_3=("What is the difference between Java & JavaScript?","What is JavaScript?","What are the data types supported by JavaScript?","What are the features of JavaScript?","Is JavaScript a case-sensitive language?","What are the advantages of JavaScript?","How can you create an object in JavaScript?","How can you create an Array in JavaScript?","What is a name function in JavaScript & how to define it?","Can you assign an anonymous function to a variable and pass it as an argument to another function?","What is argument objects in JavaScript & how to get the type of arguments passed to a function?","What are the scopes of a variable in JavaScript?","What is the purpose of ‘This’ operator in JavaScript?","What is Callback in javascript?","What is Closure in javascript? ","Name some of the built-in methods and the values returned by them in javascript.","What are the variable naming conventions in JavaScript?","How does TypeOf Operator work in javascript?","How to create a cookie using JavaScript?","How to read a cookie using JavaScript?","How to delete a cookie using JavaScript?","What is the difference between Attributes and Property in javascript?","List out the different ways an HTML element can be accessed in a JavaScript code.","In how many ways a JavaScript code can be involved in an HTML file?","What are the ways to define a variable in JavaScript?","What is a Typed language?","What is the difference between Local storage & Session storage in javascript?","What is the difference between the operators ‘==‘ & ‘===‘ in javascript?","What is the difference between null & undefined in javascript?","What is the difference between undeclared & undefined in javascript?","What some of the JavaScript Frameworks","What is the difference between window & document in JavaScript?","What is the difference between innerHTML & innerText?","What is an event bubbling in JavaScript?","What is NaN in JavaScript?","How do JavaScript primitive/object types passed in functions?","How can you convert the string of any base to integer in JavaScript?","What would be the result of 2+5+'3' in javascript?","What are Exports & Imports in javascript?","What is the ‘Strict’ mode in JavaScript and how can it be enabled?","What is a prompt box in JavaScript?","What is the difference between Call & Apply in javascript?","How to empty an Array in JavaScript? ","What are escape characters in JavaScript?","What is the reason for wrapping the entire content of a JavaScript source file in a function book?" )
+
+foreach($javascript_var in $js_array_2){
+  Invoke-WebRequest -Uri <REPLACE_ME>
+
+# $rando_time = $Random.Next(31,175);
+$rando_time = $Random.Next(5,20);
+
+sleep $rando_time;
+
+}
+
+
+
+# For Mobile Points
+
+foreach($javascript_var in $js_array_2){
+
+  Invoke-WebRequest -Uri <REPLACE_ME>
+
+# $rando_time = $Random.Next(31,175);
+$rando_time = $Random.Next(5,20);
+
+sleep $rando_time;
+
+}
+
+
